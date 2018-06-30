3fa55ebb8143eaa79b2abdf160171a6dd6bbd9a7
Dave <lobster.spam.acct@gmail.com>
2018-06-30 14:15:07 +0100

    add basic test

--- src/test/java/uk/co/bigredlobster/restStreamData/RestStreamDataApplicationTests.java
+++ src/test/java/uk/co/bigredlobster/restStreamData/RestStreamDataApplicationTests.java
@@ -1,16 +1,49 @@
 package uk.co.bigredlobster.restStreamData;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = RestStreamDataApplication.class)
+@WebAppConfiguration
 public class RestStreamDataApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    private MockMvc mockMvc;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_OCTET_STREAM.getType(),
+            MediaType.APPLICATION_OCTET_STREAM.getSubtype());
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void getsDataOk() throws Exception {
+        mockMvc.perform(get("/getDataFromFile"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType));
+    }
+
+    @Test
+    public void getsDataFail() throws Exception {
+        mockMvc.perform(get("/getDataFromFileNotThere"))
+                .andExpect(status().is4xxClientError());
+    }
 }
